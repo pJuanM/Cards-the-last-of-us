5ebc690588e0c76d5c630e08c2aeeff1dfedb7fa
Juan <105320041+pJuanM@users.noreply.github.com>
2023-03-04 11:34:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let painels = document.querySelectorAll(".painel")
+
+painels.forEach(painel => {
+    painel.addEventListener("click", () => {
+        removeActiveClasses()
+        painel.classList.add("active");
+    })
+})
+
+function removeActiveClasses() {
+    painels.forEach(painel => {
+      painel.classList.remove("active")
+    })
+}
